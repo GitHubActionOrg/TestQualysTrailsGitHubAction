0ed477b42a6a9c3b72361b9b4dc4bf045b32f013
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-23 12:05:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Owner = "Shrikant ssDhanajswade",
+    Owner = "Shrikant ssdDhanajswade",
     Name  = "Test VM QA TEST"
   }
 }
