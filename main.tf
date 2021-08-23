0eae0602245a9c10d6741482ca5a9c50f3222a65
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-23 12:35:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 resource "aws_instance" "app_serdvers" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "ami-09e67e426f2d5ce0d7"
   instance_type = "t2.micro"
 
   tags = {
