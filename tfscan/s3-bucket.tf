68012c0181a44ebee3ff36173199e6cea714b166
aphatangade <58165224+aphatangade@users.noreply.github.com>
2021-11-04 09:47:26 +0530

    Add files via upload

---
+++ tfscan/s3-bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "demo-bucket" {
+  acl    = "private"
+  bucket = "demo-bucket-20211115"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+      {
+          "Sid" : "AddCannedAclyes",
+          "Effect" : "Allow",
+          "Principal" : { "AWS" : ["184801150141"] },
+          "Action" : ["*"],
+          "Resource" : "arn:aws:s3:::demo-bucket-20211115/*",
+        }
+      ]
+    }
+  )
+}
