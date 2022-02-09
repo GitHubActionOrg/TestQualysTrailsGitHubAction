efbaa457aa5a3293196fd9ac1224980119748446
aphatangade <58165224+aphatangade@users.noreply.github.com>
2022-02-09 19:03:42 +0530

    Add files via upload

---
+++ tfscan/neptunedb-cluster-fail.tf
@@ -0,0 +1,11 @@
+resource "aws_neptune_cluster" "neptunedb-cluster-fail" {
+  cluster_identifier = "neptunedb-cluster-fail"
+  storage_encrypted  = false #checkov CKV_AWS_44
+  #enable_cloudwatch_logs_exports = ["audit"] #checkov CKV_AWS_101
+}
+
+resource "aws_neptune_cluster_instance" "neptunedb-cluster-instance-fail" {
+  cluster_identifier = aws_neptune_cluster.neptunedb-cluster-fail.id
+  instance_class     = "db.r4.small"
+  publicly_accessible = true #checkov CKV_AWS_102
+}
